228bc5863e544f7a10d19f1618d903c0f0c4a010
Florian Ehmann <florian.ehmann@gmx.de>
2017-03-05 15:48:51 +0100

    Add Main Code Files

---
+++ led_spectrum.ino
@@ -0,0 +1,18 @@
+/*
+   The following code is part of the github repository
+   https://github.com/florianehmann/led_spectrum
+
+  THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
+  ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+  PARTICULAR PURPOSE.
+
+  Copyright (c) 2017 Florian Ehmann
+*/
+
+#include "led_spectrum.h"
+
+void setup() {}
+
+void loop() {}
+
