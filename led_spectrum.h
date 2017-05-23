2b09d909288eb38405bb3171c6b86576e0764f28
Florian Ehmann <florian.ehmann@gmx.de>
2017-05-23 20:21:20 +0200

    Remove Goertzel Code

--- led_spectrum.h
+++ led_spectrum.h
@@ -18,7 +18,7 @@
 // desired maximum frequency in the spectrum in Hz
 // the current prescaling of the counter does only allow
 // values between 20 kHz and 1954 Hz
-const unsigned int MAX_FREQ = 2000;
+const unsigned int MAX_FREQ = 10000;
 
 // desired minimum frequency in the spectrum in Hz
 const unsigned int MIN_FREQ = 20;
@@ -26,8 +26,11 @@ const unsigned int MIN_FREQ = 20;
 // number of frequency bins of the output, e.g. 8 for an 8x8 matrix
 const uint8_t NUMBER_OF_BINS = 50;
 
+// number of the samples of the fft
+const uint16_t NUMBER_OF_SAMPLES= 128;
+
 // time in ms between two matrix updates
-const unsigned long MATRIX_PERIOD = 100;
+const unsigned long MATRIX_PERIOD = 100; // TODO validate if I really need this
 
 // sample frequency in Hz
 const unsigned int SAMPLE_FREQ = 2 * MAX_FREQ;
@@ -35,21 +38,9 @@ const unsigned int SAMPLE_FREQ = 2 * MAX_FREQ;
 // distance of two samples in the time domain in us
 const unsigned int SAMPLE_SPACING = (unsigned int) (1000000l / SAMPLE_FREQ);
 
-// allocate memory for the filters
-void alloc_filters();
-
-// initialize goertzel filters
-void init_filters();
-
-// acquire a sample from the adc
+// acquire a sample from the adc or simulation
 // do this inline, to save time on stack operations
 inline int acquire_sample();
-
-// feed acquired sample to the filters
-inline void feed_filters(int);
-
-// check if the filters have received their respective amount of data
-inline bool check_filters();
 
 // perform steps necessary to update the matrix
 void update_matrix();
